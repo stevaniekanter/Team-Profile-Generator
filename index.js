fc091d80bda404a0616e117ae77220a89bf9d271
Stevanie Kanter <stevaniek@yahoo.com>
2021-08-29 22:04:02 -0400

    updated html

--- index.js
+++ index.js
@@ -1,8 +1,83 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateHtml = require ("./lib/generateHtml")
 
-const question = [
+const generateHtml = (answer) =>
+  `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>My Team</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" 
+  rel="stylesheet" 
+  integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" 
+  crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 jumbotron mb-3 team-header">
+        <h1 class="text-center">My Team Member</h1>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="team-member col-12 d-flex justify-content-center";>
+        <div class="card employee-card">
+
+        // Manager information
+          <div class="card-header">
+            <h2 class="card-title managerName">{{ name }}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2 managerRole"></i>{{ role }}</h3>
+          </div>
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item managerID">ID: {{ id }}</li>
+              <li class="list-group-item managerEmail">Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
+              <li class="list-group-item managerOfficeNum">Office number: {{ officeNumber }}</li>
+            </ul>
+         </div>
+
+        // Engineer information
+          <div class="card-header">
+            <h2 class="card-title engineerName">{{ name }}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2 engineerRole"></i>{{ role }}</h3>
+          </div>
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item engineerID">ID: {{ id }}</li>
+              <li class="list-group-item engineerEmail">Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
+              <li class="list-group-item engineerGitHub">GitHub: <a href="https://github.com/{{ github }}" target="_blank" rel="noopener noreferrer">{{ github }}</a></li>
+            </ul>
+          </div>
+        </div>
+
+        // Intern information
+        <div class="card-header">
+          <h2 class="card-title internName">{{ name }}</h2>
+          <h3 class="card-title"><i class="fas fa-user-graduate mr-2 internRole"></i>{{ role }}</h3>
+        </div>
+        <div class="card-body">
+          <ul class="list-group">
+            <li class="list-group-item internID">ID: {{ id }}</li>
+            <li class="list-group-item internEmail">Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
+            <li class="list-group-item internSchool">School: {{ school }}</li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>`
+
+inquirer.propmp ([
     {
       type: "list",
       name: "manager-role",
@@ -31,16 +106,6 @@ const question = [
     },
     {
       type: "list",
-      name: "manager",
-      message: "Please enter your name",
-    },
-    {
-      type: "input",
-      name: "add-new",
-      message: "Please hit enter to add a new team member",
-    },
-    {
-      type: "list",
       name: "new-employee",
       message: "Please select the role of your team member",
       choices: ["Engineer"]
@@ -66,15 +131,10 @@ const question = [
       message: "Please enter employee Github Username",
     },
     {
-      type: "input",
-      name: "add-new2",
-      message: "Please hit enter to add a new team member",
-    },
-    {
       type: "list",
       name: "new-employee2",
       message: "Please select the role of your team member",
-      choices: ["Engineer"]
+      choices: ["Intern"]
     },
     {
       type: "list",
@@ -96,8 +156,7 @@ const question = [
       name: "intern-school",
       message: "Please enter school name",
     },
-  ];
-
+])
 
 function writeToFile(fileName, data) {
   return fs.writeFile(fileName, data, function (error) {
@@ -108,7 +167,6 @@ function writeToFile(fileName, data) {
   });
 }
 
-//initialize app
 function init() {
   inquirer.prompt(question).then((data) => {
       writeToFile("index.htlm", generateHtml({ ...data }));
