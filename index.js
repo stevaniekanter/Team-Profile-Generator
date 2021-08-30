d93c027335c334fe35521a0247edce7760292867
Stevanie Kanter <stevaniek@yahoo.com>
2021-08-29 23:16:26 -0400

    updated prompt

--- index.js
+++ index.js
@@ -1,5 +1,91 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () => {
+  return inquirer.prompt([
+  {
+    type: "list",
+    name: "manager1",
+    message: "Hi Manager, please select your role",
+    choices: ["Manager"],
+  },
+  {
+    type: "input",
+    name: "managername",
+    message: "Please enter your name",
+  },
+  {
+    type: "input",
+    name: "managerid",
+    message: "Please enter your employee ID",
+  },
+  {
+    type: "input",
+    name: "manageremail",
+    message: "Please enter your email address",
+  },
+  {
+    type: "input",
+    name: "manageroffice",
+    message: "Please enter your office number",
+  },
+  {
+    type: "list",
+    name: "newemployee",
+    message: "Please select the role of your team member",
+    choices: ["Engineer"]
+  },
+  {
+    type: "input",
+    name: "engineer",
+    message: "Please enter employee name",
+  },
+  {
+    type: "input",
+    name: "engineerid",
+    message: "Please enter employee ID",
+  },
+  {
+    type: "input",
+    name: "engineeremail",
+    message: "Please enter employee email",
+  },
+  {
+    type: "input",
+    name: "engineergithub",
+    message: "Please enter employee Github Username",
+  },
+  {
+    type: "list",
+    name: "newemployee2",
+    message: "Please select the role of your team member",
+    choices: ["Intern"]
+  },
+  {
+    type: "input",
+    name: "internname",
+    message: "Please enter intern's name",
+  },
+  {
+    type: "input",
+    name: "internid",
+    message: "Please enter intern's ID",
+  },
+  {
+    type: "input",
+    name: "internemail",
+    message: "Please enter intern's email",
+  },
+  {
+    type: "input",
+    name: "internschool",
+    message: "Please enter school name",
+  },
+]);
+};
 
 const generateHTML = (answer) =>
   `<!DOCTYPE html>
@@ -31,36 +117,36 @@ const generateHTML = (answer) =>
         <div class="card employee-card">
           <div class="card-header">
             <h2 class="card-title">${answer.manager}</h2>
-            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${answer.manager-role}</h3>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${answer.manager1}</h3>
           </div>
           <div class="card-body">
             <ul class="list-group">
-              <li class="list-group-item">ID: ${answer.manager-id}</li>
-              <li class="list-group-item">Email: <a href="mailto:${answer.manager-email}">${answer.manager-email}</a></li>
-              <li class="list-group-item">Office number: ${answer.manager-office}</li>
+              <li class="list-group-item">ID: ${answer.managerid}</li>
+              <li class="list-group-item">Email: <a href="mailto:${answer.manageremail}">${answer.manageremail}</a></li>
+              <li class="list-group-item">Office number: ${answer.manageroffice}</li>
             </ul>
           </div>
           <div class="card-header">
             <h2 class="card-title">${answer.engineer}</h2>
-            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${answer.new-employee}</h3>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${answer.newemployee}</h3>
           </div>
           <div class="card-body">
             <ul class="list-group">
-              <li class="list-group-item">ID: ${answer.engineer-id}</li>
-              <li class="list-group-item">Email: <a href="mailto:${answer.engineer-email}">${answer.engineer-email}</a></li>
-              <li class="list-group-item">GitHub: <a href="https://github.com/${answer.engineer-github}" target="_blank" rel="noopener noreferrer">${answer.engineer-github}</a></li>
+              <li class="list-group-item">ID: ${answer.engineerid}</li>
+              <li class="list-group-item">Email: <a href="mailto:${answer.engineeremail}">${answer.engineeremail}</a></li>
+              <li class="list-group-item">GitHub: <a href="https://github.com/${answer.engineergithub}" target="_blank" rel="noopener noreferrer">${answer.engineergithub}</a></li>
             </ul>
           </div>
 
           <div class="card-header">
             <h2 class="card-title">${answer.intern}</h2>
-            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${answer.new-employee2}</h3>
+            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${answer.newemployee2}</h3>
           </div>
           <div class="card-body">
             <ul class="list-group">
-              <li class="list-group-item">ID: {${answer.intern-id}</li>
-              <li class="list-group-item">Email: <a href="mailto:${answer.intern-email}">${answer.intern-email}</a></li>
-              <li class="list-group-item">School: ${answer.intern-school}</li>
+              <li class="list-group-item">ID: {${answer.internid}</li>
+              <li class="list-group-item">Email: <a href="mailto:${answer.internemail}">${answer.internemail}</a></li>
+              <li class="list-group-item">School: ${answer.internschool}</li>
             </ul>
           </div>
 
@@ -69,95 +155,13 @@ const generateHTML = (answer) =>
     </div>
   </div>
 </body>
+</html>`;
 
-</html>`
+const init = () => {
+  promptUser()
+    .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+    .then(() => console.log('Successfully wrote index.html'))
+    .catch((err) => console.error(err));
+};
 
-inquirer.prompt ([
-    {
-      type: "list",
-      name: "manager-role",
-      message: "Please select your role",
-      choices: ["Manager"],
-    },
-    {
-      type: "list",
-      name: "manager",
-      message: "Please enter your name",
-    },
-    {
-      type: "input",
-      name: "manager-id",
-      message: "Please enter your employee ID",
-    },
-    {
-      type: "input",
-      name: "manager-email",
-      message: "Please enter your email address",
-    },
-    {
-      type: "input",
-      name: "manager-office",
-      message: "Please enter your office number",
-    },
-    {
-      type: "list",
-      name: "new-employee",
-      message: "Please select the role of your team member",
-      choices: ["Engineer"]
-    },
-    {
-      type: "list",
-      name: "engineer",
-      message: "Please enter employee name",
-    },
-    {
-      type: "list",
-      name: "engineer-id",
-      message: "Please enter employee ID",
-    },
-    {
-      type: "list",
-      name: "engineer-email",
-      message: "Please enter employee emai",
-    },
-    {
-      type: "list",
-      name: "engineer-github",
-      message: "Please enter employee Github Username",
-    },
-    {
-      type: "list",
-      name: "new-employee2",
-      message: "Please select the role of your team member",
-      choices: ["Intern"]
-    },
-    {
-      type: "list",
-      name: "intern-name",
-      message: "Please enter intern's name",
-    },
-    {
-      type: "list",
-      name: "intern-id",
-      message: "Please enter intern's ID",
-    },
-    {
-      type: "list",
-      name: "intern-email",
-      message: "Please enter intern's emai",
-    },
-    {
-      type: "list",
-      name: "intern-school",
-      message: "Please enter school name",
-    },
-])
-.then((answers) => {
-  const htmlPageContent = generateHTML(answers);
-
-  fs.writeFile('index.html', htmlPageContent, (err) =>
-    err ? console.log(err) : console.log('Successfully created index.html!')
-  );
-});
-// Function call to initialize app
 init();
